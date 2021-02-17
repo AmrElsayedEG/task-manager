50488437c812dce6bedae01e3a06f9de916a7505
Amr Elsayed <newsifb@gmail.com>
2021-02-17 17:16:54 +0200

    graphql methods

--- tasks/task/schema.py
+++ tasks/task/schema.py
@@ -2,12 +2,15 @@ import graphene
 from graphene_django import DjangoObjectType
 from task.models import task
 
+#Types
 
 class TaskTybe(DjangoObjectType):
     class Meta:
         model = task
-        fields = ("id","cat_list","description","category","date")
+        fields = ("id","description","category","date")
 
+#Query
+        
 class Query(graphene.ObjectType):
     all_tasks = graphene.List(TaskTybe, id=graphene.Int())
 
@@ -17,4 +20,58 @@ class Query(graphene.ObjectType):
             tasks = task.objects.filter(id=id)
         return tasks
 
-schema = graphene.Schema(query=Query)
+
+#Mutation
+
+class AddTaskMutation(graphene.Mutation):
+    tasks = graphene.Field(TaskTybe)
+    
+    class Arguments:
+        description = graphene.String(required=True)
+        category = graphene.String(required=True)
+        date = graphene.Date(required=True)
+    
+    
+    @classmethod
+    def mutate(cls, root, info, description, category, date, *args, **kwargs):
+        tasks = task(description=description,category=category,date=date)
+        tasks.save()
+        return AddTaskMutation(tasks=tasks)
+        
+
+class UpdateTaskMutation(graphene.Mutation):
+    tasks = graphene.Field(TaskTybe)
+    
+    class Arguments:
+        id = graphene.ID()
+        description = graphene.String(required=True)
+        category = graphene.String(required=True)
+        date = graphene.Date(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, id, description, category, date, *args, **kwargs):
+        tasks = task.objects.get(id=id)
+        tasks.description = description
+        tasks.category = category
+        tasks.date = date
+        tasks.save()
+        return UpdateTaskMutation(tasks=tasks)
+
+class DeleteTaskMutation(graphene.Mutation):
+    tasks = graphene.Field(TaskTybe)
+    
+    class Arguments:
+        id = graphene.ID()
+
+    @classmethod
+    def mutate(cls, root, info, id, *args, **kwargs):
+        tasks = task.objects.get(id=id)
+        tasks.delete()
+        return
+
+class Mutation(graphene.ObjectType):
+    add_task = AddTaskMutation.Field()
+    update_task = UpdateTaskMutation.Field()
+    delete_task = DeleteTaskMutation.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
